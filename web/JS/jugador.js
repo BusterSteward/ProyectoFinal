214ff4d180a065a2a076b87697e9e823b3f53d44
BusterSteward <andres_sim97@hotmail.com>
2022-12-15 20:33:33 +0100

    movimiento terminado

--- web/JS/jugador.js
+++ web/JS/jugador.js
@@ -1,37 +1,61 @@
 "use strict";
 
 export class Jugador{
-    constructor(){
+    constructor(lab){
+        this.laberinto=lab;
         this.vida=100;
         this.danio=1;
         this.position={
             "x":-1,
             "y":-1
         }
-        this.direction=1;
+        this.direction=null;
     }
     mover(dir){
         switch(dir){
             case 1://UP
-                this.position.y++;
+                this.position.y--;
+                this.direction=1;
                 break;
             case 2://RIGHT
                 this.position.x++;
+                this.direction=2;
                 break;
             case 3://DOWN
-                this.position.y--;
+                this.position.y++;
+                this.direction=3;
                 break;
             case 4://LEFT
                 this.position.x--;
+                this.direction=4;
                 break;
         }
-        
+        console.log("x: "+this.position.x+" y: "+this.position.y);
         
     }
-    seleccionarSalida(laberinto){
-        let tam=laberinto.tamanyo;
+    girarse(){
+        switch(this.direction){
+            case 1:
+                this.direction=3;
+                break;
+            case 2:
+                this.direction=4;
+                break;
+            case 3:
+                this.direction=1;
+                break;
+            case 4:
+                this.direction=2;
+                break;
+        }
+        console.log("direccion: "+this.direction);
+    }
+    seleccionarSalida(){
+        let tam=this.laberinto.tamanyo;
         this.position.x=Math.trunc(Math.random()*tam);
         this.position.y=Math.trunc(Math.random()*tam);
-        
+        this.direction = Math.trunc(Math.random()*4)+1;
+        console.log(this.position);
+        console.log(this.direction);
     }
 }
--- web/JS/main.js
+++ web/JS/main.js
@@ -4,14 +4,55 @@ import {Jugador} from "./jugador.js";
 import{Laberinto} from "./laberinto.js";
 
 //variables globales
-var laberinto,tam=3;
-var jugador = new Jugador();
+var laberinto,tam=4;
+
 
 //inicio del juego
 function inicializarLaberinto(){
     laberinto=new Laberinto(tam);
     laberinto.generarLaberinto();
     laberinto.pintarLaberinto(canvas);
+}
+function refrescarBotones(){
+    let celda = laberinto.getCelda(jugador.position.x,jugador.position.y)
+    switch(jugador.direction){
+        case 1:
+            document.getElementById("down").classList.add("ocultar");
+            celda.walls.top ?       document.getElementById("up").classList.add("ocultar") :
+                                    document.getElementById("up").classList.remove("ocultar");
+            celda.walls.left ?      document.getElementById("left").classList.add("ocultar") :
+                                    document.getElementById("left").classList.remove("ocultar");
+            celda.walls.right ?     document.getElementById("right").classList.add("ocultar") :
+                                    document.getElementById("right").classList.remove("ocultar");
+            break;
+        case 2:
+            document.getElementById("left").classList.add("ocultar");
+            celda.walls.top ?       document.getElementById("up").classList.add("ocultar") :
+                                    document.getElementById("up").classList.remove("ocultar");
+            celda.walls.bottom ?    document.getElementById("down").classList.add("ocultar") :
+                                    document.getElementById("down").classList.remove("ocultar");
+            celda.walls.right ?     document.getElementById("right").classList.add("ocultar") :
+                                    document.getElementById("right").classList.remove("ocultar");
+            break;
+        case 3:
+            document.getElementById("up").classList.add("ocultar");
+            celda.walls.bottom ?    document.getElementById("down").classList.add("ocultar") :
+                                    document.getElementById("down").classList.remove("ocultar");
+            celda.walls.left ?      document.getElementById("left").classList.add("ocultar") :
+                                    document.getElementById("left").classList.remove("ocultar");
+            celda.walls.right ?     document.getElementById("right").classList.add("ocultar") :
+                                    document.getElementById("right").classList.remove("ocultar");
+            break;
+        case 4:
+            document.getElementById("right").classList.add("ocultar");
+            celda.walls.top ?       document.getElementById("up").classList.add("ocultar") :
+                                    document.getElementById("up").classList.remove("ocultar");
+            celda.walls.left ?      document.getElementById("left").classList.add("ocultar") :
+                                    document.getElementById("left").classList.remove("ocultar");
+            celda.walls.bottom ?    document.getElementById("down").classList.add("ocultar") :
+                                    document.getElementById("down").classList.remove("ocultar");
+            break;
+    }
 }
 
 var canvas = document.createElement("canvas");
@@ -22,15 +63,26 @@ document.querySelector("main").appendChild(canvas);
 
 document.getElementById("up").addEventListener("click",()=>{
     jugador.mover(1);
+    refrescarBotones();
 });
 document.getElementById("right").addEventListener("click",()=>{
     jugador.mover(2);
+    refrescarBotones();
 });
 document.getElementById("down").addEventListener("click",()=>{
     jugador.mover(3);
+    refrescarBotones();
 });
 document.getElementById("left").addEventListener("click",()=>{
     jugador.mover(4);
+    refrescarBotones();
 });
+
+document.getElementById("girarse").addEventListener("click",()=>{
+    jugador.girarse();
+    refrescarBotones();
+})
 inicializarLaberinto();
+var jugador = new Jugador(laberinto);
 jugador.seleccionarSalida(laberinto);
+refrescarBotones();
